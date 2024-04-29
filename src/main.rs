8ad34b4bf0a979821f806c109a058a1788be1136
Samuel Skean <sskean2@uic.edu>
2024-04-29 17:27:07 -0500

    Printing out a really simple test image!
    Lots of code copied from my raytracer.
    Oh god it's great to be able to make things show up on screen this fast.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+
+struct Resolution {
+    width: u64,
+    height: u64,
+}
+
+type PixelGrid = Vec<Vec<(u8, u8, u8)>>;
+
 fn main() {
-    println!("Hello, world!");
+    let res = Resolution {
+        width: 4,
+        height: 4,
+    };
+
+    let image: PixelGrid = vec![
+        vec![(255,0,0), (0,0,0), (0,0,0), (0,0,0)],
+        vec![(0,0,0), (0,0,0), (0,0,0), (0,0,0)],
+        vec![(0,0,0), (0,0,0), (0,0,0), (0,0,0)],
+        vec![(0,0,0), (0,0,0), (0,0,0), (0,0,127)],
+    ];
+
+
+    // PPM Image Header
+    println!("P3");
+    println!("{} {}", res.width, res.height);
+    println!("255");
+
+    // PPM Body
+    for scanline in image {
+        for pixel in scanline {
+            print!("{} {} {} ", pixel.0, pixel.1, pixel.2);
+        }
+        println!("");
+    }
 }
