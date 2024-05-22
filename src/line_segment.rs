a71339ae737a1c5864b2251a4f8ca365ffbae15e
Samuel Skean <sskean2@uic.edu>
2024-05-21 22:45:33 -0500

    fixup! Made the curves and lines smoother!

--- src/line_segment.rs
+++ src/line_segment.rs
@@ -10,7 +10,6 @@ const LERP_RESOLUTION_FOR_LINE_SEGMENTS: u64 = 1_000;
 
 impl Draw for LineSegment {
     fn draw(&self, target: &mut PixelGrid) {
-        // TODO: Why do my lines come out all weird, even if LERP_RESOLUTION is as high as it can matter? Is that really just an artifact of the limited resolution (observed on a 400*400 image).
         for integral_t in 0..LERP_RESOLUTION_FOR_LINE_SEGMENTS {
             let t = integral_t as f64/LERP_RESOLUTION_FOR_LINE_SEGMENTS as f64;
             let point_on_line = lerp(self.p0, self.p1, t);
